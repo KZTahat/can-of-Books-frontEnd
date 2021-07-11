d65ff2c2f8a67ea62705c520f53afef650127ee1
KZTahat <kztahat96@gmail.com>
2021-07-11 19:16:34 +0300

    fixing login button

--- src/Header.js
+++ src/Header.js
@@ -10,7 +10,7 @@ import { withAuth0 } from '@auth0/auth0-react';
 class Header extends React.Component {
   render() {       
     // console.log(this.props);
-    const isAuthenticated = this.props.auth0;    
+    const isAuthenticated = this.props.auth0.isAuthenticated;    
     return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
